8ac3ae30e716fdd3a4e8466577fe89839f22c58c
abhilipsa7381 <71922350+abhilipsa7381@users.noreply.github.com>
2022-04-09 22:35:10 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+package file.configuration;
+
+public class Main {
+	
+	public static final String path = "C:\\Users\\abnayak\\resources"; 
+	
+
+	public static void main(String[] args) {
+		
+				Menu menu = new Menu();
+				menu.introScreen();
+				menu.mainMenu();
+	}
+}
+
+
+
